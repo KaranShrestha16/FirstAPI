cfe169fbaf40feb3be2952df906cd07a2e28fa5d
Softwarica College Lab <softwarica@softwarica.com>
2019-05-12 12:07:58 +0545

    Complite Employee Register

--- app/src/main/java/ClientAPI/EmployeeAPI.java
+++ app/src/main/java/ClientAPI/EmployeeAPI.java
@@ -1,16 +1,40 @@
 package ClientAPI;
 
 import java.util.List;
+
+import model.EmployeeCUD;
 import model.Employees;
 import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
 import retrofit2.http.Path;
 
 public interface EmployeeAPI {
 
+//  To Get all the employees details
       @GET("employees")
       Call<List<Employees>> getAllEMployess();
 
+      // Get employee on basic of employee id
       @GET("employee/{empID}")
       Call<Employees> getEmployeeByID(@Path("empID") int empID);
+
+      // update employee on basis of emp id
+      @PUT("employee/{empID}")
+      Call<Void> updateEmployees(@Path("empID") int empId);
+
+      // Add Employee to Web API
+      @POST("create")
+      Call<Void> registerEmployee(@Body EmployeeCUD emp);
+
+      // Delete employee on the basic of emp id
+      @DELETE("delete/{empID}")
+      Call<Void> deleteEmployee(@Path ("empID") int empId);
+
+
+
+
 }
---
+++ app/src/main/java/com/example/firstapi/EmployeeRegistration.java
@@ -0,0 +1,69 @@
+package com.example.firstapi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import ClientAPI.EmployeeAPI;
+import model.EmployeeCUD;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class EmployeeRegistration extends AppCompatActivity {
+
+    private final static String BASE_URL ="http://dummy.restapiexample.com/api/v1";
+    private EditText etName, etSalary,etAge;
+    private Button btnRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employee_registration);
+
+        etAge=findViewById(R.id.etAge);
+        etSalary=findViewById(R.id.etSearch);
+        etName=findViewById(R.id.etName);
+        btnRegister=findViewById(R.id.btnRegister);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Register();
+            }
+        });
+    }
+
+    private void Register(){
+
+        EmployeeCUD emp= new EmployeeCUD(etName.getText().toString(),Float.parseFloat(etSalary.getText().toString()),Integer.parseInt(etAge.getText().toString()));
+
+        Retrofit retrofit= new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        EmployeeAPI employeeAPI = retrofit.create(EmployeeAPI.class);
+        Call<Void> voidCall = employeeAPI.registerEmployee(emp);
+        voidCall.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                Toast.makeText(EmployeeRegistration.this,"You have been sucessfully registered" ,Toast.LENGTH_LONG).show();
+
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+
+                Toast.makeText(EmployeeRegistration.this,"ERROR" + t.getLocalizedMessage(),Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+}
